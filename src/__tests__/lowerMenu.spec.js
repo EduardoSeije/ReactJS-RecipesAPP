abad04f606b1c194d5b0f0d52910ba4a48bebfb5
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-06-30 11:58:18 -0300

    [tests] | requisito-20

--- src/__tests__/lowerMenu.spec.js
+++ src/__tests__/lowerMenu.spec.js
@@ -16,19 +16,19 @@ const user = {
   foodBottomBtn: 'food-bottom-btn',
 };
 
+const { getByTestId } = renderWithRouter(<App />);
+
+const inputEmail = getByTestId(user.inputEmailTestId);
+userEvent.type(inputEmail, user.validEmail);
+
+const inputPassword = getByTestId(user.inputPasswordTestId);
+userEvent.type(inputPassword, user.passwordValid);
+
+const btnSubmit = getByTestId(user.loginSubmitBtnTestId);
+userEvent.click(btnSubmit);
+
 describe('19 - Implemente os elementos do menu inferior respeitando'
 + 'os atributos descritos no protótipo', () => {
-  const { getByTestId } = renderWithRouter(<App />);
-
-  const inputEmail = getByTestId(user.inputEmailTestId);
-  userEvent.type(inputEmail, user.validEmail);
-
-  const inputPassword = getByTestId(user.inputPasswordTestId);
-  userEvent.type(inputPassword, user.passwordValid);
-
-  const btnSubmit = getByTestId(user.loginSubmitBtnTestId);
-  userEvent.click(btnSubmit);
-
   it('O menu inferior deve ter possuir o atributo data-testid="footer"', () => {
     const footer = getByTestId(user.footerTestId);
     expect(footer).toBeInTheDocument();
@@ -50,5 +50,33 @@ describe('19 - Implemente os elementos do menu inferior respeitando'
   + 'o atributo data-testid="food-bottom-btn"', () => {
     const btnFoods = getByTestId(user.foodButtomBtn);
     expect(btnFoods).toBeInTheDocument();
+  });
+});
+
+describe('20 - Posicione o menu inferior de forma fixa e apresente 3'
++ 'ícones: um para comidas, um para bebidas e outro para exploração', () => {
+  it('O menu inferior deve ficar fixado sempre ao final da página', () => {
+    const footer = getByTestId(user.footerTestId);
+    expect(footer).toHaveStyle('position: absolute; bottom: 0');
+  });
+
+  it('Apresenta os ícones corretos', () => {
+    const btnDrinks = getByTestId(user.drinksButtonBtn);
+    const imgDrinks = getByRole(btnDrinks, 'img', {
+      name: /([/|.|\w|\s|-])(drinkicon)([/|.|\w|\s|-])*\.(?:svg)/i,
+    });
+    expect(imgDrinks).toBeInTheDocument();
+
+    const btnExplore = getByTestId(user.exploreButtomBtn);
+    const imgExplore = getByRole(btnExplore, 'img', {
+      name: /([/|.|\w|\s|-])(exploreicon)([/|.|\w|\s|-])*\.(?:svg)/i,
+    });
+    expect(imgExplore).toBeInTheDocument();
+
+    const btnFoods = getByTestId(user.foodBottomBtn);
+    const imgFoods = getByRole(btnFoods, 'img', {
+      name: /([/|.|\w|\s|-])(mealicon)([/|.|\w|\s|-])*\.(?:svg)/i,
+    });
+    expect(imgFoods).toBeInTheDocument();
   });
 });
