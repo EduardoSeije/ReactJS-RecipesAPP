aedb727b2b5ecef58e674f7a924a9c9084d502a8
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-07-03 13:21:45 -0300

    :sparkles: Requirement 41

--- src/components/DetailsRecipe/StartRecipeButton.js
+++ src/components/DetailsRecipe/StartRecipeButton.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+import AppContext from '../../contexts/app/AppContext';
 
 export default function StartRecipeButton(props) {
+  const { screenActive } = useContext(AppContext);
+  const history = useHistory();
   const { item } = props;
+  const type = screenActive === 'food' ? 'comidas' : 'bebidas';
 
   return (
     item.length
       ? (
         <Button
           data-testid="start-recipe-btn"
+          onClick={ () => {
+            history.push(
+              `/${type}/${screenActive === 'food'
+                ? item[0].idMeal
+                : item[0].idDrink}/in-progress`,
+            );
+          } }
         >
           Iniciar Receita
         </Button>
