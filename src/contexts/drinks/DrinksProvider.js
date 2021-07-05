fbf42ef074461f32bc31b0d23c8fa994a4682e74
lucas-chaves <lcchavesr150@gmail.com>
2021-07-05 18:11:34 -0300

    fix conflits

--- src/contexts/drinks/DrinksProvider.js
+++ src/contexts/drinks/DrinksProvider.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import resultApiDrinks from '../../service/fetchAPIDrinks';
-
 import DrinksContext from './DrinksContext';
 
 function DrinksProvider({ children }) {
