d6c1e6fcc692789b83e861676ff18df4a4438254
Eduardo Seije <eduardoseije@gmail.com>
2021-07-05 15:49:38 -0300

    Feat:Req.14 - início do requisito

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,16 +6,29 @@ function SearchBar() {
       <label htmlFor="search-input">
         <input data-testid="search-input" id="search-input" type="text" />
       </label>
-      <label htmlFor="search-radio">
+      <label htmlFor="search-ingredient">
+        <input
+          name="search-radio"
+          data-testid="ingredient-search-radio"
+          id="search-ingredient"
+          type="radio"
+        />
+        Ingrediente
+      </label>
+      <label htmlFor="search-name">
         <input
           name="search-radio"
           data-testid="name-search-radio"
+          id="search-name"
           type="radio"
         />
         Busca pelo nome
+      </label>
+      <label htmlFor="search-first-letter">
         <input
           name="search-radio"
           data-testid="first-letter-search-radio"
+          id="search-first-letter"
           type="radio"
         />
         Busca pela primeira letra
