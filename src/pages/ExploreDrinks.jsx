bf5d3b85f4ae8f14050476ee12f3256f28f0798f
Eduardo Seije <eduardoseije@gmail.com>
2021-07-07 19:56:43 -0300

    início

---
+++ src/pages/ExploreDrinks.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExploreDrinks = () => (
+  <div>
+    <p>explore drinks</p>
+  </div>
+);
+
+export default ExploreDrinks;
---
+++ src/pages/ExploreFood.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExploreFood = () => (
+  <div>
+    <p>explore food</p>
+  </div>
+);
+
+export default ExploreFood;
