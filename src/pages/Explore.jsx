ee215a6e821894e1cefc2a92cf8033f749902ad3
Eduardo Seije <eduardoseije@gmail.com>
2021-07-07 19:11:25 -0300

    tela explorar

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LowerMenu from '../components/LowerMenu';
 import Header from '../components/Header';
 
@@ -6,7 +7,12 @@ export default function Explore() {
   return (
     <div>
       <Header />
-      <h1>Explore</h1>
+      <Link to="/explorar/comidas">
+        <button data-testid="explore-food" type="button">Explorar Comidas</button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button data-testid="explore-drinks" type="button">Explorar Bebidas</button>
+      </Link>
       <LowerMenu />
     </div>
   );
