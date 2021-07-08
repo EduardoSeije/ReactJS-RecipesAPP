5ee0096639aa6b86e7a9e7b6945536c703a5968d
Eduardo Seije <eduardoseije@gmail.com>
2021-07-08 15:19:33 -0300

    tela explorar comidas e bebidas

--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const ExploreFood = () => (
-  <div>
-    <Link to="/explorar/comidas/ingredientes">
-      <button
-        data-testid="explore-by-ingredient"
-        type="button"
-      >
-        Por Ingredientes
-      </button>
-    </Link>
-    <Link to="/explorar/comidas/area">
-      <button
-        data-testid="explore-by-area"
-        type="button"
-      >
-        Por Local de Origem
-      </button>
-    </Link>
-    {/* <Link to=""> */}
-    <button
-      data-testid="explore-surprise"
-      type="button"
-    >
-      Me Surpreenda!
-    </button>
-    {/* </Link> */}
-  </div>
-);
+function ExploreFood() {
+  const [randomMeal, setRandomMeal] = useState([]);
+  async function requestRandomMeal() {
+    const fetchAPI = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const response = await fetchAPI.json();
+    // console.log(response);
+    setRandomMeal(response.meals[0].idMeal);
+  }
+
+  useEffect(() => {
+    requestRandomMeal();
+  }, []);
+
+  return (
+    <div>
+      {/* {console.log(random)} */}
+      <Link to="/explorar/comidas/ingredientes">
+        <button
+          data-testid="explore-by-ingredient"
+          type="button"
+        >
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <button
+          data-testid="explore-by-area"
+          type="button"
+        >
+          Por Local de Origem
+        </button>
+      </Link>
+      <Link to={ randomMeal !== undefined ? `/comidas/${randomMeal}` : null }>
+        <button
+          data-testid="explore-surprise"
+          type="button"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
+    </div>
+  );
+}
 
 export default ExploreFood;
