8db92885f8ed744aa993d9dd45a7b4b1c1bbdf09
Eduardo Seije <eduardoseije@gmail.com>
2021-07-09 18:21:33 -0300

    ajuste req 17

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
   const { categoriesFoods, mealsRecipes, setFiltersFoods,
     filtersFoods, setToggleFoods, toggleFoods } = useContext(MealsProvider);
 
+    
   return (
     <div className="meals">
       <Switch>
